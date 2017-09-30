ece9ecdb91c61e34b399b2f20ef87f57ac9bbf44
kostaivanov <kostasvetlozarov@gmail.com>
2017-10-01 01:04:26 +0300

    C# CONDITIONAL STATEMENTS AND LOOPS - EXERCISES

---
+++ 10.TestNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        char startLetter = char.Parse(Console.ReadLine());
+        char endLetter = char.Parse(Console.ReadLine());
+        string skip = Console.ReadLine();
+        string result = "";
+
+        for (char i = startLetter; i <= endLetter; i++)
+        {
+            for (char j = startLetter; j <= endLetter; j++)
+            {
+                for (char k = startLetter; k <= endLetter; k++)
+                {
+                    result = $"{i}{j}{k}";
+                    if (!result.Contains(skip))
+                    {
+                        Console.Write(result + " ");                         
+                    }
+                }
+            }
+        }
+    }
+}
